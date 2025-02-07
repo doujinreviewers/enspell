0b5ab675ec1c4cebb69bfb01855326718ebc771d
doujinreviewers <doujin.reviewers@gmail.com>
2025-02-07 21:36:30 +0900

    fix search

--- src/content/search.js
+++ src/content/search.js
@@ -3,9 +3,6 @@
   const baseFilterModule = await import(chrome.runtime.getURL('content/base_filter.js'));
   const { filterCells, DLSITE_ENSPELL_STORAGE_KEY } = baseFilterModule;
 
-  let type1_cells = document.querySelectorAll(".work_1col_table.n_worklist > tbody > tr");
-  let type3_cells = document.querySelectorAll("#search_result_img_box > li");
-
   chrome.storage.local.get(DLSITE_ENSPELL_STORAGE_KEY, (data)=>{
     let settings = data[DLSITE_ENSPELL_STORAGE_KEY] || {};
 
@@ -13,15 +10,33 @@
       return;
     }
 
-    let cells = type1_cells.length != 0 ? type1_cells : type3_cells;
-    let ngcount = filterCells(cells, settings.ng_circles);
+    let dom = document.querySelector("#main_inner");
 
-    if (settings.show_ng_count) {
-      let page_total = document.querySelector(".page_total");
-      if(page_total){
-        page_total.innerHTML = page_total.innerHTML + `<strong>-${ngcount}</strong>件`
+    let observerCallback = function (record, observer) {
+      observer.disconnect();
+
+      let type1_cells = document.querySelectorAll(".work_1col_table.n_worklist > tbody > tr");
+      let type3_cells = document.querySelectorAll("#search_result_img_box > li");
+  
+      let cells = type1_cells.length != 0 ? type1_cells : type3_cells;
+      let ngcount = 0;
+      if(cells){
+        ngcount = filterCells(cells, settings.ng_circles);
       }
+  
+      if (settings.show_ng_count) {
+        let page_total = document.querySelector(".base_title_br .original_name");
+        if(page_total && ngcount > 0){
+          page_total.innerHTML = `検索結果 <strong>-${ngcount}</strong>件`
+        }
+      }
+
+      observer.observe(dom, { childList: true, subtree: true });
     }
+
+    let observer = new MutationObserver(observerCallback);
+    observerCallback([], observer);
+    observer.observe(dom, {childList: true,subtree: true});
 
     let works_type_ranking = document.querySelector("#_works_type_ranking");
 
